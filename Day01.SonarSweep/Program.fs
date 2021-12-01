4ad2be1fe66a79143aed4a3f0a919145e48cfcc0
Willem Sporen <willem.sporen@scenius.nl>
2021-12-01 16:53:39 +0100

    Day 1 - 1*

--- Day01.SonarSweep/Program.fs
+++ Day01.SonarSweep/Program.fs
@@ -1,4 +1,28 @@
+open System.IO
 
+let ReadPuzzleInput file =
+    file
+    |> File.ReadAllLines
+    |> Array.map (int)
+    |> Array.toList
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let NumberOfIncreases (input: int list) =
+    input
+    |> List.skip 1
+    |> List.zip (input |> List.rev |> List.skip 1 |> List.rev)
+    |> List.fold (fun acc (previous, current) -> if current > previous then acc + 1 else acc) 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "*- Advent of Code 2021 -*"
+    printfn "|  Day 01  Sonar Sweep  |"
+    printfn "-------------------------"
+    match Array.length argv with
+    | x when x = 1 ->
+        match File.Exists(argv[0]) with
+        | true ->
+            let numberOfIncreases = NumberOfIncreases (ReadPuzzleInput argv[0])
+            printfn $"[*] Number of increases {numberOfIncreases}"
+            0
+        | _ -> failwith "File not found"
+    | _ -> failwith "Usage: ./dotnet <path-to-puzzle-input>"
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Advent of Code 2021
 
-My own solutions to the Advent of Code 2021 problems.
-With the goal of experimenting a bit with new languages and tools!
+My own solutions to the Advent of Code 2021 problems. With the goal of experimenting a bit with new languages and tools!
 
-| Day | Language | Progress       | Solution |
-| --- | -------- | --------        | -------- |
-|  1  | F#       |  | [Day01.ReportRepair](https://github.com/Lerke/AdventOfCode2021/tree/main/Day01.ReportRepair)
+| Day | Language | Progress | Solution |
+| --- | -------- |----------| -------- |
+|  1  | F#       | :star2:  | [Day01.SonarSweep](https://github.com/Lerke/AdventOfCode2021/tree/main/Day01.SonarSweep)
