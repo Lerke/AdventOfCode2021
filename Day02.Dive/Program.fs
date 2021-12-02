af2a45694a55e8e0199ae737c965892ab22e5615
Willem <willem.sporen@scenius.nl>
2021-12-02 23:18:28 +0100

    Day 2 - 1 star

---
+++ Day02.Dive/Program.fs
@@ -0,0 +1,51 @@
+open System.IO
+open System.Text.RegularExpressions
+
+type SubmarineDirection =
+    | Up of depth: int
+    | Down of depth: int
+    | Forward of movement: int
+
+let (|Distance|_|) str =
+   match Regex("(?<distance>\d+)").Match(str) with
+   | m when m.Success -> Some(int m.Groups["distance"].Value)
+   | _ -> None
+
+let (|ParseDirection|) str =
+    match str with
+    | Distance value when str.StartsWith("forward") -> Forward value
+    | Distance value when str.StartsWith("up") -> Down value
+    | Distance value when str.StartsWith("down") -> Up value
+    | _ -> failwith $"Could not parse {str}"
+
+let CalculatePosition directions =
+    directions
+    |> List.fold
+        (fun acc current -> match current with
+                            | Up x -> {| HorizontalPosition = acc.HorizontalPosition; Depth =  acc.Depth + x |}
+                            | Down x -> {| HorizontalPosition = acc.HorizontalPosition; Depth =  acc.Depth - x |}
+                            | Forward x -> {| HorizontalPosition = acc.HorizontalPosition + x; Depth =  acc.Depth |} ) {| HorizontalPosition = 0; Depth = 0  |}
+
+let ReadPuzzleInput file =
+    file
+    |> File.ReadAllLines
+    |> Array.where (fun f -> (System.String.IsNullOrEmpty(f) |> not))
+    |> Array.map (fun f -> match f with
+                           | ParseDirection f -> f)
+    |> Array.toList
+
+[<EntryPoint>]
+let main argv =
+    printfn "*- Advent of Code 2021 -*"
+    printfn "|     Day 02   Dive     |"
+    printfn "-------------------------"
+    match Array.length argv with
+    | x when x = 1 ->
+        match File.Exists(argv[0]) with
+        | true ->
+            let input = ReadPuzzleInput argv[0]
+            let finalPosition = CalculatePosition input
+            printf $"[*] Horizontal Position: {finalPosition.HorizontalPosition}, Depth: {finalPosition.Depth}, Multiplied: {finalPosition.HorizontalPosition * finalPosition.Depth}"
+            0
+        | _ -> failwith "File not found"
+    | _ -> failwith "Usage: ./dotnet <path-to-puzzle-input>"
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 My own solutions to the Advent of Code 2021 problems. With the goal of experimenting a bit with new languages and tools!
 
-| Day | Language | Progress        | Solution |
-| --- | -------- |-----------------| -------- |
+| Day | Language | Progress      | Solution |
+| --- | -------- |---------------| -------- |
 |  1  | F#       | :star2: :star2: | [Day01.SonarSweep](https://github.com/Lerke/AdventOfCode2021/tree/main/Day01.SonarSweep)
+|  1  | F#       | :star2: | [Day02.Dive](https://github.com/Lerke/AdventOfCode2021/tree/main/Day02.Dive)
