f90b95c793ac5f01e133596d4eb58e1fa9e632aa
Willem <willemsporen@gmail.com>
2024-11-28 16:27:45 +0100

    Day 3 - Rework

--- Day03.BinaryDiagnostic/Program.fs
+++ Day03.BinaryDiagnostic/Program.fs
@@ -1,42 +1,45 @@
 open System
+open System.Collections.Generic
 open System.IO
 
-let BitCountAtPosition (input:char list list) position =
-    input
-    |> List.map (fun f -> f[position])
-    |> List.groupBy id
+type DiagnosticReport(rows: int array array) =
+    member val Rows = rows
+
+    member this.Column(i: int) : int seq =
+        seq { 0 .. this.Rows.Length - 1 } |> Seq.map (fun f -> this.Rows[f][i])
+
+    member this.CountBitsForColumn(i: int) : Dictionary<int, int> =
+        this.Column(i)
+        |> Seq.fold
+            (fun acc curr ->
+                match acc.TryGetValue curr with
+                | (true, f) ->
+                    (acc[curr |> int] <- f + 1)
+                    acc
+                | (false, _) ->
+                    (acc.Add(curr |> int, 1))
+                    acc)
+            (Dictionary<int, int>())
+
 
 let ReadPuzzleInput file =
     file
     |> File.ReadAllLines
     |> Array.where (fun f -> (String.IsNullOrEmpty(f) |> not))
-    |> Array.map  List.ofSeq
-    |> Array.toList
-
-let bInt (input: char list) =
-    (Convert.ToInt32((String.Concat(Array.ofList(input))), 2))
+    |> Array.map(fun f -> f |> Seq.map(fun s -> s |> int) |> Seq.toArray)
+    |> DiagnosticReport
 
 [<EntryPoint>]
 let main argv =
     printfn "--- Day 3: Binary Diagnostic ---"
+
     match Array.length argv with
     | x when x = 1 ->
         match File.Exists(argv[0]) with
         | true ->
             let input = ReadPuzzleInput argv[0]
-            let bits = seq { 0 .. (input |> List.head |> List.length) - 1 } |> Seq.toList
-                       |> List.map (BitCountAtPosition input)
-            let mostCommonBits = bits
-                                 |> List.map (List.maxBy (snd >> List.length))
-                                 |> List.map (fst)
-            let leastCommonBits = bits
-                                 |> List.map (List.minBy (snd >> List.length))
-                                 |> List.map (fst)
-
-            printfn "Most common bits: %i in decimal\nLeast common bits: %i in decimal\nPower consumption: %i"
-                (mostCommonBits |> bInt)
-                (leastCommonBits |> bInt)
-                ((mostCommonBits |> bInt) * (leastCommonBits |> bInt))
+            let colOne = input.CountBitsForColumn(0)
+            printf "A"
             0
         | _ -> failwith "File not found"
     | _ -> failwith "Usage: ./dotnet <path-to-puzzle-input>"
