eb591d4c62b963bce014ee4a3d29e611c0543737
Willem <willemsporen@gmail.com>
2024-11-29 14:54:14 +0100

    Day 3 - Rework

--- Day03.BinaryDiagnostic/Program.fs
+++ Day03.BinaryDiagnostic/Program.fs
@@ -2,6 +2,12 @@ open System
 open System.Collections.Generic
 open System.IO
 
+let BinToDec (binary: int seq) : int =
+    binary
+    |> Seq.rev
+    |> Seq.zip (seq { 0 .. (binary |> Seq.length) - 1 })
+    |> Seq.fold (fun acc curr -> acc ||| ((curr |> snd) <<< (curr |> fst))) 0
+
 type DiagnosticReport(rows: int array array) =
     member val Rows = rows
 
@@ -14,19 +20,39 @@ type DiagnosticReport(rows: int array array) =
             (fun acc curr ->
                 match acc.TryGetValue curr with
                 | (true, f) ->
-                    (acc[curr |> int] <- f + 1)
+                    (acc[curr] <- f + 1)
                     acc
                 | (false, _) ->
-                    (acc.Add(curr |> int, 1))
+                    (acc.Add(curr, 1))
                     acc)
             (Dictionary<int, int>())
 
+    member this.MostCommonBitsPerColumn() : int[] =
+        seq { 0 .. this.Rows[0].Length - 1 }
+        |> Seq.map (fun f ->
+            let count = this.CountBitsForColumn(f)
+
+            match ((count.TryGetValue 0), (count.TryGetValue 1)) with
+            | (true, z), (true, o) -> if z > o then 0 else 1
+            | (false, _), (true, _) -> 1
+            | (true, _), (false, _) -> 0
+            | (false, _), (false, _) -> failwithf "empty column")
+        |> Seq.toArray
+
+    member this.Gamma() : int =
+        this.MostCommonBitsPerColumn() |> BinToDec
+
+    member this.Epsilon() : int =
+        this.MostCommonBitsPerColumn() |> Seq.map (fun f -> f ^^^ 1) |> BinToDec
+    
+    member this.PowerConsumption() : int =
+        this.Gamma() * this.Epsilon()
 
 let ReadPuzzleInput file =
     file
     |> File.ReadAllLines
     |> Array.where (fun f -> (String.IsNullOrEmpty(f) |> not))
-    |> Array.map(fun f -> f |> Seq.map(fun s -> s |> int) |> Seq.toArray)
+    |> Array.map (fun f -> f |> Seq.map (fun s -> Int32.Parse(s |> string)) |> Seq.toArray)
     |> DiagnosticReport
 
 [<EntryPoint>]
@@ -38,8 +64,9 @@ let main argv =
         match File.Exists(argv[0]) with
         | true ->
             let input = ReadPuzzleInput argv[0]
-            let colOne = input.CountBitsForColumn(0)
-            printf "A"
+            printfn "Gamma %A" (input.Gamma())
+            printfn "Epsilon %A" (input.Epsilon())
+            printfn "Power Consumption: %A" (input.PowerConsumption())
             0
         | _ -> failwith "File not found"
     | _ -> failwith "Usage: ./dotnet <path-to-puzzle-input>"
