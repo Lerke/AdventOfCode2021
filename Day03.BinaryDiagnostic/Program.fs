793aaf534aa12cddfc5df583c8926508fd736cdc
Willem <willemsporen@gmail.com>
2024-11-30 04:38:11 +0100

    Day 3 - 2 stars

--- Day03.BinaryDiagnostic/Program.fs
+++ Day03.BinaryDiagnostic/Program.fs
@@ -8,35 +8,40 @@ let BinToDec (binary: int seq) : int =
     |> Seq.zip (seq { 0 .. (binary |> Seq.length) - 1 })
     |> Seq.fold (fun acc curr -> acc ||| ((curr |> snd) <<< (curr |> fst))) 0
 
+let Column (i: int, rows: int array array) : int seq =
+    seq { 0 .. rows.Length - 1 } |> Seq.map (fun f -> rows[f][i])
+
+let MostCommonBit (count: Dictionary<int, int>) : int =
+    match ((count.TryGetValue 0), (count.TryGetValue 1)) with
+    | (true, z), (true, o) when z > o -> 0
+    | (true, z), (true, o) when z < o -> 1
+    | (true, z), (true, o) when z = o -> 1
+    | (false, _), (true, _) -> 1
+    | (true, _), (false, _) -> 0
+    | (false, _), (false, _) -> failwithf "empty column"
+    | _ -> failwith "Unsupported"
+
+let CountBitsForColumn (i: int, rows: int array array) : Dictionary<int, int> =
+    Column(i, rows)
+    |> Seq.fold
+        (fun acc curr ->
+            match acc.TryGetValue curr with
+            | true, f ->
+                (acc[curr] <- f + 1)
+                acc
+            | false, _ ->
+                acc.Add(curr, 1)
+                acc)
+        (Dictionary<int, int>())
+
 type DiagnosticReport(rows: int array array) =
     member val Rows = rows
-
-    member this.Column(i: int) : int seq =
-        seq { 0 .. this.Rows.Length - 1 } |> Seq.map (fun f -> this.Rows[f][i])
-
-    member this.CountBitsForColumn(i: int) : Dictionary<int, int> =
-        this.Column(i)
-        |> Seq.fold
-            (fun acc curr ->
-                match acc.TryGetValue curr with
-                | (true, f) ->
-                    (acc[curr] <- f + 1)
-                    acc
-                | (false, _) ->
-                    (acc.Add(curr, 1))
-                    acc)
-            (Dictionary<int, int>())
 
     member this.MostCommonBitsPerColumn() : int[] =
         seq { 0 .. this.Rows[0].Length - 1 }
         |> Seq.map (fun f ->
-            let count = this.CountBitsForColumn(f)
-
-            match ((count.TryGetValue 0), (count.TryGetValue 1)) with
-            | (true, z), (true, o) -> if z > o then 0 else 1
-            | (false, _), (true, _) -> 1
-            | (true, _), (false, _) -> 0
-            | (false, _), (false, _) -> failwithf "empty column")
+            let count = CountBitsForColumn(f, this.Rows)
+            MostCommonBit(count))
         |> Seq.toArray
 
     member this.Gamma() : int =
@@ -44,9 +49,41 @@ type DiagnosticReport(rows: int array array) =
 
     member this.Epsilon() : int =
         this.MostCommonBitsPerColumn() |> Seq.map (fun f -> f ^^^ 1) |> BinToDec
-    
-    member this.PowerConsumption() : int =
-        this.Gamma() * this.Epsilon()
+
+    member this.PowerConsumption() : int = this.Gamma() * this.Epsilon()
+
+    member this.LifeSupportRatingComponent(selector: int -> int -> bool, ?rows: int array array, ?column: int) =
+        let rowsToUse =
+            match rows with
+            | Some r -> r
+            | None -> this.Rows
+
+        let columnToUse =
+            match column with
+            | Some c -> c
+            | None -> 0
+
+        match rowsToUse with
+        | [| x |] -> BinToDec x
+        | x ->
+            rowsToUse
+            |> (fun f ->
+                let count = CountBitsForColumn(columnToUse, f)
+
+                match MostCommonBit(count) with
+                | i ->
+                    this.LifeSupportRatingComponent(
+                        selector,
+                        x |> Array.filter (fun f -> selector f[columnToUse] i),
+                        columnToUse + 1
+                    ))
+
+    member this.OxygenGeneratorRating() = this.LifeSupportRatingComponent((=))
+
+    member this.Co2ScrubberRating() = this.LifeSupportRatingComponent((<>))
+
+    member this.LifeSupportRating() =
+        this.OxygenGeneratorRating() * this.Co2ScrubberRating()
 
 let ReadPuzzleInput file =
     file
@@ -64,9 +101,8 @@ let main argv =
         match File.Exists(argv[0]) with
         | true ->
             let input = ReadPuzzleInput argv[0]
-            printfn "Gamma %A" (input.Gamma())
-            printfn "Epsilon %A" (input.Epsilon())
-            printfn "Power Consumption: %A" (input.PowerConsumption())
+            printfn $"⭐\tPower Consumption:\t%A{input.PowerConsumption()}"
+            printfn $"⭐⭐\tLife Support Rating:\t%A{input.LifeSupportRating()}"
             0
         | _ -> failwith "File not found"
     | _ -> failwith "Usage: ./dotnet <path-to-puzzle-input>"
---
+++ Day03.BinaryDiagnostic/readme.md
@@ -0,0 +1,6 @@
+```shell
+$ dotnet run ./puzzle-input.txt
+--- Day 3: Binary Diagnostic ---
+⭐       Power Consumption:      198
+⭐⭐     Life Support Rating:    230
+```
